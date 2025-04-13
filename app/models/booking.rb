19228133acb10c973d7800f0841babe91944ae9b
Mihir Desai <mihir@mihirdesai.net>
2025-04-13 19:03:04 +0530

    Add booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,22 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ticket_type
+
+  validate :check_ticket_availability, on: :create
+
+  private
+
+  def check_ticket_availability
+    return unless ticket_type && quantity
+
+    ticket_type.with_lock do
+      booked = ticket_type.bookings.sum(:quantity)
+      available = ticket_type.quantity - booked
+
+      if quantity > available
+        errors.add(:quantity, "Insufficent tickets available. Only #{available} tickets left.")
+        throw :abort
+      end
+    end
+  end
 end
